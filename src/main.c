b530411a17fd5999f8f364a2c01b5b6c671f16a9
nullspaceIndex <208178755+nullspaceIndex@users.noreply.github.com>
2025-07-08 23:15:17 +0200

    new build structure

---
+++ src/main.c
@@ -0,0 +1,13 @@
+
+#define CLAY_IMPLEMENTATION
+#include "clay.h"
+#include "raylib.h"
+//#include "clay_renderer_raylib.c"
+#include <stdio.h>
+
+
+
+int main (void){
+    printf("Hello World");
+    return 0;
+}
