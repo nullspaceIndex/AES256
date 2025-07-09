ec85f87910eba610952071aa58832f9ecca634e7
nullspaceIndex <208178755+nullspaceIndex@users.noreply.github.com>
2025-07-09 12:42:48 +0200

    added Layout structure + AES file for encryption

--- src/main.c
+++ src/main.c
@@ -4,32 +4,78 @@
 #include "raylib.h"
 #include "clay_renderer_raylib.c"
 //#include <stdio.h>
-
-
+#define BLUE {50, 76, 168,255}
+#define GREEN {62, 168, 50,255}
+#define GREY {100, 93, 105, 255}
+#define WHITE {255, 255, 255, 255}
 
 int main (void){
-
     const Font defaultFont = GetFontDefault();
 
     Clay_Raylib_Initialize(1024, 768, "Test",FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
     u_int64_t clayRequiredMemory = Clay_MinMemorySize();
-    Clay_Arena clayMemory = (Clay_Arena){
-        .memory = malloc(clayRequiredMemory),
-        .capacity = clayRequiredMemory
-    };
+    Clay_Arena clayArena = Clay_CreateArenaWithCapacityAndMemory(clayRequiredMemory, malloc(clayRequiredMemory));
+    
 
-    Clay_Initialize(clayMemory, (Clay_Dimensions){ 1024,768}, (Clay_ErrorHandler){Clay__ErrorHandlerFunctionDefault});
-
+    Clay_Initialize(clayArena, (Clay_Dimensions){ 1024,768}, (Clay_ErrorHandler){Clay__ErrorHandlerFunctionDefault});
 
     while(!WindowShouldClose()){
 
     Clay_BeginLayout();
 
+        
+    CLAY({
+        .id = CLAY_ID("OuterContainer"), 
+        .layout = { .sizing = {CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0)}, 
+        .padding = CLAY_PADDING_ALL(60)}, 
+        .backgroundColor = BLUE }) {
+        
+        CLAY({
+                .id = CLAY_ID("Header"),
+                .layout = {
+                .sizing = { .width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_FIXED(60) }, 
+                .padding = CLAY_PADDING_ALL(16), 
+                .childGap = 16 },
+                
+                .cornerRadius = CLAY_CORNER_RADIUS(10),
+                .backgroundColor = GREEN
+            }) {
+                CLAY({
+                    .id = CLAY_ID("EncryptButton"),
+
+                }){}
+                CLAY({
+
+                }){}
+                CLAY({
+
+                }){}    
+            }
+        
+        CLAY({
+            .id = CLAY_ID("Password"),
+            .layout = {.sizing = {CLAY_SIZING_FIT(0), CLAY_SIZING_PERCENT(10)}, },
+            .backgroundColor = WHITE
+        }){}
+        
+        CLAY({
+            .id = CLAY_ID("InputField"),
+            .layout = {.sizing = {CLAY_SIZING_FIT(0), CLAY_SIZING_PERCENT(10)}, },
+            .backgroundColor = WHITE
+        }){}
+        CLAY({
+            .id = CLAY_ID("EncryptedText"),
+            .layout = {.sizing = {CLAY_SIZING_FIT(0), CLAY_SIZING_PERCENT(10)}, },
+            .backgroundColor = WHITE
+        }){}
+    }
+
+
 
 
     Clay_RenderCommandArray clay_RenderCommands = Clay_EndLayout();
     
-
+    
     BeginDrawing();
     ClearBackground(BLACK);
     Clay_Raylib_Render(clay_RenderCommands, &defaultFont);
@@ -37,7 +83,6 @@ int main (void){
     
     EndDrawing();
     
-
     
 
 };
